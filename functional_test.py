f72591df921343678652cf3a40567f044b633acf
81275918 <81275918@qq.com>
2017-04-20 17:51:27 +0800

    home.html

--- functional_test.py
+++ functional_test.py
@@ -6,12 +6,14 @@ Created on 2017年3月30日
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
-from distutils.dist import warnings
+from pydoc import browse
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
         
     def tearDown(self):
         self.browser.quit()
@@ -19,7 +21,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn("To-Do", self.browser.title)
-        self.fail('Finish the test!')
+        head_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", head_text)
 #伊迪丝听说有一个很酷的在线待办事项应用
 #她去看了这个应用的首页
 
@@ -30,12 +33,20 @@ class NewVisitorTest(unittest.TestCase):
 
 
 #应用邀请她输入一个待办事项
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 #她在一个文本框中输入了“Buy peacock feathers”
 #伊迪丝的爱好是使用假蝇做钓饵
+        inputbox.send_keys('Buy peacock feathers')
 
 #她按回车键后，页面刷新了
 #待办事项表格中显示了“1：Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1：Buy peacock feathers' for row in rows))
+        self.fail('Finish the test!')
+
 
 #页面中又显示了一个输入框，可以输入其他的待办事项
 #她输入“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,23 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    
+    def test_root_url_resolve_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
+        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        '''
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
+        '''
+        excepted_html = render_to_string('home.html')
+        self.assertEquals(response.content.decode(), excepted_html)
